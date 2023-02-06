afcc7ab2d06077561ff458ff2be30696b3a69f7a
Karel Ježík <117266468+Keristrasz@users.noreply.github.com>
2023-02-06 18:48:52 +0000

    added badge

--- src/index.tsx
+++ src/index.tsx
@@ -5,5 +5,12 @@ import App from './App'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <a
+      id="badge"
+      target="_blank"
+      href="https://github.com/Keristrasz/1-5-Clock---React"
+    >
+      <div>Code on GitHub</div>
+    </a>
   </React.StrictMode>
 )
